ad6a2150f020735706cb4feb6d9dec14d5922822
Cris Lom <bigc4997@gmail.com>
2021-01-04 08:58:31 -0500

    improved express settings for server

--- router.js
+++ router.js
@@ -3,36 +3,34 @@ const createError = require('http-errors');
 const express = require('express');
 const https = require('https');
 const http = require('http');
-const fs = require('fs');
-
 const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+// const usersRouter = require('./routes/users');
+// const catalogRouter = require('./routes/catalog');
 
 const app = express();
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, './')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// const port = 8080;
-const options = {
+app.use('./', indexRouter);
+
+
+// const options = {
     // key: fs.readFileSync('pemKey'),
     // cert: fs.readFileSync('pemKey')
-  };
+//   };
 
 
-app.get('/test666', (req, res) => {
-    res.sendFile(path.join(`${__dirname}/test666.html`));
-});
-
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(`${__dirname}/index.html`));
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
   });
 
-app.use(function(err, req, res, next) {
+  // error handler
+  app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -43,5 +41,4 @@ app.use(function(err, req, res, next) {
   });
 
 
-  http.createServer(app).listen(8080)
-//   https.createServer(options, app).listen(443)
+module.exports = app;
