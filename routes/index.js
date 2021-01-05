6472428a047b2ebefed3d28894e30c16be59103e
Cris Lom <bigc4997@gmail.com>
2021-01-04 23:11:39 -0500

    improved

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
+
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
     console.log('indexPARTY PANTS');
     res.sendFile(path.join(`${__dirname}/home.html`));
-    // res.redirect('/catalog');
 });
 
 module.exports = router;
